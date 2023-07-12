0dbe82ca448560d5ec1cdce5be46ab6e6535c4f3
FloresCaetano <caetano.flores@hotmail.com>
2023-07-12 15:21:47 -0500

    BetsabeBoutique interfaces

---
+++ 06-Code/BethsabeBoutiqueV0.8.2/src/main/java/ec/edu/espe/bethsabeboutique/view/Main.java
@@ -0,0 +1,14 @@
+package ec.edu.espe.bethsabeboutique.view;
+
+/**
+ *
+ * @author caeta
+ */
+public class Main {
+    public static void main(String[] args) {
+       PnlLogin loginPanel = new PnlLogin();
+       loginPanel.setVisible(true);
+       loginPanel.pack();
+       loginPanel.setLocationRelativeTo(null);
+    }
+}
